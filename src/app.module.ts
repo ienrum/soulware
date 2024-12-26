b6fd98d05995e14e83c56e0bf59d6bc79c8afa0e
sue hyeok chae <137467530+ienrum@users.noreply.github.com>
2024-12-26 13:54:24 +0900

    feat: postgres 설정 및 cat entity 추가가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Cat } from 'src/cats/entities/cat.entity';
 @Module({
-  imports: [CatsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'test',
+      password: 'test',
+      database: 'test',
+      synchronize: true,
+      logging: true,
+      entities: [],
+    }),
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
