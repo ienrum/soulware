a1e84c81e4b39086d827783b4c164201d9d6ed00
sue hyeok chae <137467530+ienrum@users.noreply.github.com>
2025-01-03 11:03:27 +0900

    feat: AuthGuard 를 사용해 수정,삭제 전 확인

--- src/threads/threads.controller.ts
+++ src/threads/threads.controller.ts
@@ -7,29 +7,34 @@ import {
   Post,
   Put,
   Query,
-  Redirect,
-  Req,
+  UseGuards,
 } from '@nestjs/common';
-import { Request } from 'express';
 import { ThreadsService } from 'src/threads/threads.service';
 import { CreateThreadDto } from 'src/threads/dtos/create-thread.dto';
-import { GetDocsQueryList } from 'src/threads/dtos/query-docs';
 import { UpdateThreadDto } from 'src/threads/dtos/update-thread.dto';
 import { Thread } from 'src/threads/entities/thread.entity';
 import { ResponseThreadListDto } from 'src/threads/dtos/thread.response.dto';
 import { PaginationQuryDto } from 'src/threads/dtos/pagenation.query.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { GetUserId } from 'src/auth/decorators/get-userid.decorator';
 @Controller('threads')
 export class ThreadsController {
   constructor(private threadsService: ThreadsService) {}
 
+  @UseGuards(AuthGuard)
   @Post()
   create(@Body() createThreadDto: CreateThreadDto) {
     this.threadsService.create(createThreadDto);
   }
 
+  @UseGuards(AuthGuard)
   @Put(':id')
-  update(@Param('id') id: number, @Body() updateThreadDto: UpdateThreadDto) {
-    this.threadsService.update(id, updateThreadDto);
+  update(
+    @Param('id') id: number,
+    @Body() updateThreadDto: UpdateThreadDto,
+    @GetUserId() userid: number,
+  ) {
+    this.threadsService.update(id, updateThreadDto, userid);
   }
 
   @Get()
@@ -44,8 +49,9 @@ export class ThreadsController {
     return await this.threadsService.findOne(id);
   }
 
+  @UseGuards(AuthGuard)
   @Delete(':id')
-  delete(@Param('id') id: string) {
-    this.threadsService.delete(id);
+  delete(@Param('id') id: number, @GetUserId() userid: number) {
+    return this.threadsService.delete(id, userid);
   }
 }
