47602044fcffd7c126dc457b2d75ee05cb297555
sue hyeok chae <137467530+ienrum@users.noreply.github.com>
2025-01-08 13:23:06 +0900

    feat: signupDto password 유효성 검사

--- src/auth/dto/user-signup.dto.ts
+++ src/auth/dto/user-signup.dto.ts
@@ -1,4 +1,4 @@
-import { MaxLength, MinLength } from 'class-validator';
+import { Matches, MaxLength, MinLength } from 'class-validator';
 
 export class UserSignUpDto {
   @MinLength(3, { message: 'Name is too short' })
@@ -7,5 +7,12 @@ export class UserSignUpDto {
 
   @MinLength(8, { message: 'Password is too short' })
   @MaxLength(20, { message: 'Password is too long' })
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[a-zA-Z\d!@#$%^&*]{8,}$/,
+    {
+      message:
+        'Password must contain at least one uppercase letter, one lowercase letter, and one number or special character',
+    },
+  )
   password: string;
 }
