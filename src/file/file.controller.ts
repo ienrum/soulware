422e65a9ecfc7808ab89ad63c9cf6b096f49c016
sue hyeok chae <137467530+ienrum@users.noreply.github.com>
2025-01-16 16:58:13 +0900

    feat: guard 추가 및 response dto 추가

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,22 +1,31 @@
 import {
+  Body,
+  ClassSerializerInterceptor,
   Controller,
   Delete,
   Get,
+  Logger,
   Param,
   Post,
   Res,
   UploadedFiles,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { Response } from 'express';
 import { diskStorage } from 'multer';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { GetUserId } from 'src/auth/decorators/get-userid.decorator';
+import { FileListResponseDto } from 'src/file/dtos/file.response';
 import { FileService } from 'src/file/file.service';
 
+@UseInterceptors(ClassSerializerInterceptor)
 @Controller('file')
 export class FileController {
   constructor(private readonly fileService: FileService) {}
 
+  @UseGuards(AuthGuard)
   @Post('upload/:threadId')
   @UseInterceptors(
     FilesInterceptor('files', 10, {
@@ -32,13 +41,20 @@ export class FileController {
   uploadFiles(
     @UploadedFiles() files: Array<Express.Multer.File>,
     @Param('threadId') threadId: number,
+    @GetUserId() userId: number,
   ) {
     return this.fileService.uploadFiles(files, threadId);
   }
 
   @Get(':threadId')
-  getFiles(@Param('threadId') threadId: number) {
-    return this.fileService.getFiles(threadId);
+  async getFiles(
+    @Param('threadId') threadId: number,
+    @GetUserId() userId: number,
+  ) {
+    const fileList = await this.fileService.getFiles(threadId);
+
+    new Logger(FileController.name).log(JSON.stringify(fileList), userId);
+    return new FileListResponseDto(fileList, userId);
   }
 
   @Get('download/:filename')
